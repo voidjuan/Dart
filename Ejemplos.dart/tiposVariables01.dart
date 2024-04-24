2688aa7baaaffb848c228af4a2fa4ca5347086c4
monin18 <juan_elopez2@soy.sena.edu.co>
2024-04-24 11:44:44 -0500

    DART

---
+++ Ejemplos.dart/tiposVariables01.dart
@@ -0,0 +1,22 @@
+void main () {
+  print ("Hello ADSO 2873711");
+  /*
+  TIPOS DE VARIABLES:
+  String: Tipo cadena
+  int: Tipo numero entero
+  double: Tipo numero real
+  bool: Tipo logico o booleano
+  */
+  String nombre; //Variable tipo cadena
+  int edad; // Variable tipo numero
+  double altura; // Variable de numero real
+  bool estado; // Variable logica
+
+  nombre = "Ana"; // Asignacion de variables
+  edad = 18; // Asignacion de varables
+  altura = 1.70; // Asignacion de variables
+  estado = true; //Asignacion de variables
+
+  print ("Su nombre es $nombre"); //Salida de variables
+  print ("Su altura es $altura y su edad es $edad"); //Salida de variables
+}
---
+++ Ejemplos.dart/tiposVariables02.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+void main() {
+  print("Tipos de variables");
+  print("Tipo ENTERO");
+  int cantExamenes; //Definicion
+  cantExamenes = 3; //Asignacion
+  int cantEstudiantes = 30; //Definicion y asignacion de variables
+  print("La cantidad de estudiantes es $cantEstudiantes");
+  print("Deben presentar $cantExamenes examenes");
+  print("**************************");
+  print("Tipo REAL");
+  double peso; //Definicion
+  peso = 70.5; //Asignacion
+  double promedio = 3.5; //Definicion y asignacion de variables
+  print("Su peso es $peso y su promedio es: $promedio");
+  print("**************************");
+  print("Tipo CADENA");
+  String telefono;
+  telefono = 3015325284;
+  String email = "jelopez702@gmail.com";
+  print("Su telefono es $telefono y su email es $email");
+  print("**************************");
+  print("Tipo BOOLEANO");
+  bool esCasado;
+  esCasado = false;
+  Bool tieneHijos = true;
+  print("Es casado: $esCasado");
+  print("Tiene hijostouch: $tieneHijos");
+}
