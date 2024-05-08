ecec46a16f2115c45bf5a082bd80070deb160a9f
voidjuan <juan_elopez2@soy.sena.edu.co>
2024-05-08 16:53:17 -0500

    Ejemplos WHILE

--- Ciclos/EjemplosCiclos/ejemploWHILE_03.dart
+++ Ciclos/EjemplosCiclos/ejemploWHILE_03.dart
@@ -1,14 +1,25 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  int num = 0, suma = 0;
-  double promedio;
+  int n, contador = 0;
+  double numero, promedio, suma = 0;
 
-  
-  while(num > 0) {
-  print("Ingrese el numero (negativo para salir)");
-  num = int.parse(stdin.readLineSync()!);
-  suma = suma + num;
-  promedio = suma /
+  print("Ingrese la cantidad de numeros que desea promediar");
+  n = int.parse(stdin.readLineSync()!);
+   while (contador < n) {
+    print('Ingrese el número ${contador + 1}:');
+    numero = double.parse(stdin.readLineSync()!);
+    if(numero > 0) {
+      suma += numero;
+      contador++;
+    } else {
+      print("El numero ingresado no es positivo, por favor ingresa un numero positivo");
+    }
+   }
+    if (contador > 0) {
+    promedio = suma / contador;
+    print('La suma de los $n numeros es: $suma, y el promedio es: $promedio');
+  } else {
+    print('No se ingresaron números positivos para calcular el promedio.');
   }
 }
